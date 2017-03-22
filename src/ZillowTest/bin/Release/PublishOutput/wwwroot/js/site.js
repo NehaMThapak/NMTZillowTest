ced5f7b34f6dd7742264499d66a966251233aef8
Neha <Neha>
2017-03-21 22:54:55 -0700

    First commit

---
+++ src/ZillowTest/bin/Release/PublishOutput/wwwroot/js/site.js
@@ -0,0 +1,10 @@
+﻿  
+$(function () { 
+    $("#ButtonSearch").click(function (e) {
+        e.preventDefault(); 
+        var href = "/Home/SearchResult?street=" + encodeURIComponent($("#Street").val()) + "&city=" + $("#City").val() + "&State=" + $("select#selectStateSpan option:checked").val() + "&Zipcode=" + $("#Zipcode").val();
+        $("a").attr("href", href);
+        $('#' + $(this).data("target")).load($(this).attr("href")); 
+    });
+});
+
